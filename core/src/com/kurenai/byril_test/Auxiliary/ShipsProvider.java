042f40904653b576bdbdaf3796f0edc3393be43c
kkurenai <vladislavkurenai@gmail.com>
2020-07-25 00:42:42 +0300

    deleted unused comments

--- core/src/com/kurenai/byril_test/Auxiliary/ShipsProvider.java
+++ core/src/com/kurenai/byril_test/Auxiliary/ShipsProvider.java
@@ -8,31 +8,25 @@ import static com.kurenai.byril_test.Auxiliary.Constants.SHIPS_SHEET_ATLAS_PATH;
 
 public class ShipsProvider {
 
-    //private TextureRegion shipsSheet;
     private TextureAtlas textureAtlas;
 
     public ShipsProvider(){
-        //shipsSheet = new TextureRegion(new Texture(Gdx.files.internal(SHIPS_SHEET_PATH)));
         textureAtlas = new TextureAtlas(Gdx.files.internal(SHIPS_SHEET_ATLAS_PATH));
     }
 
     public Ship get1DeckShip() {
-        //return new Ship(new TextureRegion(shipsSheet, 0, 0, 32, 32));
         return new Ship(textureAtlas.findRegion("1deckship"));
     }
 
     public Ship get2DecksShip() {
-        //return new Ship(new TextureRegion(shipsSheet, 0, 0, 32, 63));
         return new Ship(textureAtlas.findRegion("2decksship"));
     }
 
     public Ship get3DecksShip() {
-       // return new Ship(new TextureRegion(shipsSheet, 32 + 6, 0, 32, 94));
         return new Ship(textureAtlas.findRegion("3decksship"));
     }
 
     public Ship get4DecksShip() {
-        //return new Ship(new TextureRegion(shipsSheet, 112 , 0, 32, 125));
         return new Ship(textureAtlas.findRegion("4decksship"));
     }
 
